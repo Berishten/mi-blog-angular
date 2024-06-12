26872404dd1eb0fa30d94d20690a5210febc6534
Pablo <beridevs@gmail.com>
2024-06-12 13:08:21 -0400

    Se crean bases de componente detalle post y lista posts

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../models/post.model';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { BlogService } from '../../services/blog.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css'
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit {
+  post: Post | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private blogService: BlogService
+  ) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.blogService.getPostById(id).subscribe(post => this.post = post);
+  }
 }
--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Post } from '../../models/post.model';
+import { BlogService } from '../../services/blog.service';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
+  posts: Post[] = [];
 
+  constructor(private blogService: BlogService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.blogService.getPosts().subscribe(posts => this.posts = posts);
+  }
+
+  showPostDetail(post: Post): void {
+    this.router.navigate(['/post', post.id]);
+  }
 }
